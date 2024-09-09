ffbb7149ed1fc1d881b2d6ce0bbbf805091f8675
Cristian Medina <medinacristian@gmail.com>
2024-09-09 00:29:24 -0300

    Add more comments

--- src/cl/duoc/laboraty_inventory/ClDuocLaboraty_inventory.java
+++ src/cl/duoc/laboraty_inventory/ClDuocLaboraty_inventory.java
@@ -4,10 +4,12 @@ public class ClDuocLaboraty_inventory {
     public static void main(String[] args) {
         // Proporciona la ruta del archivo CSV donde se guardarán los productos
         String rutaArchivoCSV = "src/csv/materiales_laboratorio.csv";
+        // Crear una instancia de Inventario
         Inventario inventario = new Inventario(rutaArchivoCSV);
 
         // Inicializar el menú principal
         MenuPrincipal menu = new MenuPrincipal(inventario);
+        // Mostrar el menú principal
         menu.mostrarMenu();
     }
 }
